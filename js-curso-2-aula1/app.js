08f40d69cf728f12e94e8d01da62d41dc66f3ffc
Jonathan Alexandre <jonathanalexandrev@gmail.com>
2025-08-19 00:19:39 -0300

    Update app.js

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -7,6 +7,8 @@ let tentativas = 1;
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', 
+    {rate:1.2});
 }
 
 function exibirMensagemInicial() {
@@ -72,6 +74,7 @@ function reiniciarJogo() {
     exibirMensagemInicial();
     document.getElementById('reiniciar').setAttribute('disabled', true)
 }
+
 
 
 
